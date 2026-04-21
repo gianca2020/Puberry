15a0ba0ed025cf07a96dfd8f601ffa6eb9b7412f
gianca2020 <giancarloforero@gmail.com>
2026-04-20 23:52:50 -0400

    use coin PNG asset via import instead of SVG

--- frontend/src/components/CoinIcon.jsx
+++ frontend/src/components/CoinIcon.jsx
@@ -1,7 +1,9 @@
+import coinPng from '../assets/coin-svgrepo-com.png';
+
 export default function CoinIcon({ className = 'w-5 h-5' }) {
   return (
     <img
-      src="/coin-svgrepo-com.svg"
+      src={coinPng}
       alt="coin"
       className={className}
       aria-hidden="true"
